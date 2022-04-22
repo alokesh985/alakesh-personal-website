e14130d11b8d7fd660ce91bc82e52e98612b0629
Alakesh Bora <alokesh@Alakeshs-MacBook-Air.local>
2022-04-22 18:44:52 +0530

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import Container from "./components/Container";
-import { useEffect } from "react";
 
 function App() {
-  useEffect(() => {
-    document.title = "Alakesh Bora";
-  }, []);
   return <Container />;
 }
 
